dad0c38302a64fed3ed7df8506f4d7b91649b4a1
Ezaan Mangalji <ezaanmangalji@quad2-366.wireless-resnet.upenn.edu>
2016-12-08 23:22:12 -0500

    Initial commit

---
+++ spec-viewer/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  spec-viewer
+//
+//  Created by Ezaan Mangalji on 2016-12-08.
+//  Copyright © 2016 Ezaan Mangalji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var pickedImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func photoLibraryAction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!){
+        pickedImage.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    
+
+}
+
